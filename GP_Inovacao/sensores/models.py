c197ebf78cd36fa4b877e9d604653ca2c67fa1b3
Bolinfel <bolinfel@gmail.com>
2024-11-07 20:19:10 -0300

    Ajeitei e fiz tabela

---
+++ GP_Inovacao/sensores/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Local(models):
+    MaqLin = models.CharField(verbose_name="Maquina ou Linha", max_length=3)
+    Setor = models.CharField(verbose_name="Setor", max_length=20)
+    Linha = models.CharField(verbose_name="Linha", max_length=20)
+    Obs = models.CharField(verbose_name="Observações", max_length=30)
+
+class Sensor(models):
+    FK_Local = models.ForeignKey(Local, verbose_name="Local", on_delete=models.CASCADE)
+
+class Leituras(models):
+    FK_Sensor = models.ForeignKey(Sensor, verbose_name="Sensor", on_delete=models.CASCADE)
+    ValorA = models.FloatField(verbose_name="Valor Lido A")
+    ValorB = models.FloatField(verbose_name="Valor Lido B")
+    Data = models.DateTimeField(verbose_name="Data da Leitura")
---
+++ GP_Inovacao/sensores/views.py
@@ -0,0 +1,4 @@
+from django.views.generic import TemplateView
+
+class Sensores(TemplateView):
+    template_name = "sensores.html"
